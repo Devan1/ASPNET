4903043be39b33182f2507ba3919f9b1b0eedba3
Devan Fields <24418547+Devan1@users.noreply.github.com>
2022-03-22 20:46:28 -0500

    completed setting up project

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Testing
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+
+namespace Testing
 {
     public interface IProductRepository
     {
+        public IEnumerable<Product> GetAllProducts();
     }
 }
--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -1,13 +1,21 @@
-﻿using System.Data;
+﻿using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+using Dapper;
 
 namespace Testing
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
         private readonly IDbConnection _conn;
         public ProductRepository(IDbConnection conn)
         {
             _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
         }
     }
 }
