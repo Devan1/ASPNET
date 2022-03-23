00fb4a94ac524b9f1c2d7e8b72812eb19df05807
Devan Fields <24418547+Devan1@users.noreply.github.com>
2022-03-22 20:13:31 -0500

    working on project

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace Testing
+{
+    public class ProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+    }
+}
